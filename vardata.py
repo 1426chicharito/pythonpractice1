c924db1ba1e31a35f1eca261e201c4161c6507e0
1426chicharito <37340612+1426chicharito@users.noreply.github.com>
2018-03-20 00:33:00 +0530

    Update vardata.py

--- vardata.py
+++ vardata.py
@@ -1,8 +1,6 @@
 def celc_far(a):
     if   a<-273.15:
         print("The lowest possible temperature that physical matter can reach is -273.15 °")
-    elif a == 0:
-        print("the temperature in Fahrenheit is 32")
     else:
         b = 32 + 1.8*a
         print("the temperature in Fahrenheit is {0}".format(b))
